0a78546364cd69e0ad1b430e2b5a39ab41138f87
schwarzer-geiger <lalitmistry1407@gmail.com>
2023-10-21 21:27:21 +0100

    not compiling

--- v1code/v1code.ino
+++ v1code/v1code.ino
@@ -1,23 +1,32 @@
 // create motor structs with attributes
-
-struct dc {
-  int powerPort;
-  int enc1Pin;
-  int enc2Pin;
-}
-
-struct stepper {
-  int dirPin;
-  int stepPin;
-}
-
-void setup() {
-  // Define input variables here
-  #include <AFMotor.h>
+#include <AFMotor.h>
+#include <Encoder.h>
 
   // Macros
   #define CW 0
   #define CCW 1
+
+class dc {
+public:
+  int powerPort;
+  int enc1Pin;
+  int enc2Pin;
+
+  dc(int pP, int e1P, int e2P) {
+    powerPort = pP;
+    enc1Pin = e1P;
+    enc2Pin = e2P;
+    Encoder enc(enc1Pin, enc2Pin);
+  }
+};
+
+struct stepper {
+  int dirPin;
+  int stepPin;
+};
+
+void setup() {
+  // Define input variables here
   
   // create motors
   struct dc motor1;
@@ -26,6 +35,7 @@ void setup() {
   motor1.powerPort = 1;
   motor1.enc1Pin = 2;
   motor1.enc2Pin = 3;
+  motor1.enc = Encoder(motor1.enc1Pin, motor1.enc2Pin = 3);
 
   motor2.powerPort = 1;
   motor2.enc1Pin = 2;
@@ -34,21 +44,32 @@ void setup() {
   AF_DCMotor m1(motor1.powerPort);
   AF_DCMotor m2(motor2.powerPort);
 
+
   // enter all the dimensions
 }
 
-void moveNStepsDC(struct dc motor, int nSteps, int dir, int speed) {
-  int stepsCompleted = 0;
+// void moveNStepsDC(struct dc motor, int nSteps, int dir, int speed) {
+//   motor.setSpeed(speed);
+//   motor.run(dir);
+//   initPos = motor.enc.read()
+//   desiredStepDelta = 
+//   while (motor.enc.read() - initPos != (2*dir - 1)*nSteps) {  }
+//   motor.run(release);
+//   motor.enc.write(0);
+// }
+
+void moveNStepsDC(AF_DCMotor motor, int nSteps, int dir, int speed);
+
+moveNStepsDC(motor, nSteps, dir, speed) {
   motor.setSpeed(speed);
-  while (stepsCompleted < nSteps) {
-    motor.run(dir);
-    
-  }
+  motor.run(dir);
+  while (abs(motor.enc.read()) < nSteps) {  }
   motor.run(release);
+  motor.enc.write(0);
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  moveNStepsDC(m1, 2000, FORWARD, 200);
 
 }
